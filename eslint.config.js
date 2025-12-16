144b67c4dd7ff3b7d35c44286af1e805306e8d59
Vivek Kumar Dwivedi <vivekdwivedi498@gmail.com>
2025-12-17 00:56:04 +0530

    Added basic project setup

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import pluginJs from '@eslint/js'
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from "globals";
+export default [
+  { 
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.node },
+    plugins:{
+      "simple-import-sort": simpleImportSort
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error"
+    }
+  },
+  pluginJs.configs.recommended
+];
